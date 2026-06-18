e54bdd1acabf6f29535239d08149f75dce3e13e4
luismedinasaucedo <medinasaucedoluiscarlo@gmail.com>
2026-06-18 14:42:25 -0600

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 25             # integer
+print(type(variable))
+variable = 98.6   # floating-point number
+print(type(variable))
+variable = True       # boolean
+print(type(variable))
+variable = "Alice"       # string
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
